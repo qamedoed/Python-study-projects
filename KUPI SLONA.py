290a294ac718e2dfd838301bed7fb8f684936615
qamedoed <zyka9569@gmail.com>
2025-03-01 18:16:27 +0500

    Completed version of the "Buy an Elephant".
    A humorous teaser program for reinforcing knowledge of working with loops using "for".

---
+++ KUPI SLONA.py
@@ -0,0 +1,29 @@
+# Задача 1. Доска
+# Используя новые знания с циклами и оператором end, выведете на экран “доску”, которую вы делали в первом модуле
+
+print("-" * 10)
+for i in range(10):
+    if i == 0 or i == 9:
+        print("|", end="")
+    else:
+        print("0", end="")
+print()
+for i in range(10):
+    if i == 0 or i == 9:
+        print("|", end="")
+    else:
+        print("0", end="")
+print()
+for i in range(10):
+    if i == 0 or i == 9:
+        print("|", end="")
+    else:
+        print("0", end="")
+print()
+for i in range(10):
+    if i == 0 or i == 9:
+        print("|", end="")
+    else:
+        print("0", end="")
+print()
+print("-" * 10)
